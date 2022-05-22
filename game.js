a701f5125470e76aa3af2388236efc1eb104235b
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-05-22 14:37:33 -0500

    semi-final product

--- game.js
+++ game.js
@@ -9,12 +9,24 @@ function docReady(fn) {
 }
 
 docReady(function() {
-    debugger;
     setNamesFromQueryString();
 });
 
+const quizAnswers={1:"Finding Nemo", 2:"Michael", 3:"New Hampshire", 4:"Kanye West", 5:"Avatar", 6:"201", 7:"Europe and Asia", 8:"Lady Gaga", 9:"Mary Poppins", 10:"Pennsylvania", 11:"Canada", 12:"Chevrolet"}
+
+
+
 function quest(questionNum) { 
-    let numberOfQuestions = 12;
+    let numberOfQuestions = 13;
+    const buttons = document.querySelectorAll('.q1btn');
+    buttons.forEach((button, index) =>{
+        if (questionNum == index+1 || button.classList.contains("answeredQuest")) {
+            button.setAttribute("disabled", true);
+            button.classList.remove("red");
+            button.classList.add("answeredQuest")
+        }
+    })
+   
     for (let index = 1; index < numberOfQuestions; index++) {
         let elementId = "q" + index;
         if(questionNum === index){
@@ -26,7 +38,6 @@ function quest(questionNum) {
 }
 
 function setNamesFromQueryString() {
-    debugger;
     const params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
       });
@@ -45,13 +56,16 @@ function setNamesFromQueryString() {
 }
 
 function submitAnswer(questionNum, questionAmount){
-    debugger;
     var currentPlayer = getCurrentPlayer();
-
-    //do something with the answer such as calculate score
-    setCurrentPlayerScore(questionAmount);
-
-    //dimm out already asked questions
+    
+    let ele = document.querySelectorAll(".answer")
+        for (let i = 0; i < ele.length; i++) {
+            if (ele[i].checked){
+                answer = ele[i].value
+              
+            }
+        }
+    setCurrentPlayerScore(questionNum, questionAmount, answer);
 
     setNewPlayerTurn(currentPlayer);
 }
@@ -67,20 +81,27 @@ function setNewPlayerTurn(currentPlayer) {
     }
 }
 
-function setCurrentPlayerScore(questionAmount) {
+function setCurrentPlayerScore(questionNum, questionAmount, answer) {
     var currentPlayer = getCurrentPlayer();
-
     var currentScore = 0;
-
-    if(currentPlayer === '1') {
+    console.log(typeof quizAnswers[questionNum])
+    if(currentPlayer === '1' && answer === quizAnswers[questionNum]) {
         currentScore = document.getElementById("playerOneScore").innerHTML;
-        document.getElementById("playerOneScore").innerHTML = currentScore + questionAmount;
-    } else {
+        document.getElementById("playerOneScore").innerHTML = +currentScore + +questionAmount;
+    } else if(answer === quizAnswers[questionNum]) {
         currentScore = document.getElementById("playerTwoScore").innerHTML;
-        document.getElementById("playerTwoScore").innerHTML = currentScore + questionAmount;
+        document.getElementById("playerTwoScore").innerHTML = +currentScore + +questionAmount;
     }
 }
 
 function getCurrentPlayer() {
     return document.getElementById("currentPlayer").value;
 }
+
+let oneWins = document.getElementById("playerOneWins")
+let twoWins = document.getElementById("playerTwoWins")
+    if (button.classList.contains("answeredQuest") == false && "playerOneScore" > "playerTwoScore") {
+        document.getElementById(oneWins).style.display="block";
+    } else {
+        document.getElementById(twoWins).style.display="block";
+    }
